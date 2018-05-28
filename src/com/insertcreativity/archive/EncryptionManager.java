beabf53346f7bdc77d713a6d085b1cd68d7ae498
Austin Henriksen <austin.r.henriksen79@gmail.com>
2018-05-27 21:02:33 -0400

    Finished encryption engine

--- src/com/insertcreativity/archive/EncryptionManager.java
+++ src/com/insertcreativity/archive/EncryptionManager.java
@@ -7,7 +7,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.io.Serializable;
-import java.math.BigInteger;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.charset.Charset;
@@ -18,9 +17,10 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Arrays;
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
-import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public final class EncryptionManager implements Serializable
@@ -30,7 +30,7 @@ public final class EncryptionManager implements Serializable
     static{
         serialVersionUID = (new SecureRandom()).nextLong();
     }
-    
+
     /**
      * Prevent potential attackers from cloning this object.
     **/
@@ -38,7 +38,7 @@ public final class EncryptionManager implements Serializable
     {
         throw new CloneNotSupportedException();
     }
-    
+
     /**
      * Prevent potential attackers from serializing this object.
     **/
@@ -46,7 +46,7 @@ public final class EncryptionManager implements Serializable
     {
         throw new IOException("Object cannot be serialized.");
     }
-    
+
     /**
      * Prevent potential attackers from deserializing this object.
     **/
@@ -54,11 +54,11 @@ public final class EncryptionManager implements Serializable
     {
         throw new IOException("Object cannot be serialized.");
     }
-    
-    /**The buffer size to use when encrypting data.**/
-    private final int bufferSize = 65536;
-    /**The initialization vector used by the encryption manager.**/
-    private final BigInteger iv = new BigInteger(new byte[] {77, 97, 100, 105, 65, 117, 115, 116, 105, 110, 82, 97, 99, 104, 101, 108});
+
+    /**The internal counter used by the encryption manager.**/
+    private final byte[] counter;
+    /**The number of the block that the manager is currently over.**/
+    private final long currentPosition;
     /**Engine used for performing hash functions.**/
     private final MessageDigest hashEngine1;
     /**Engine used for performing hash functions.**/
@@ -69,74 +69,291 @@ public final class EncryptionManager implements Serializable
     private final SecureRandom randomEngine;
     /**The character encoding scheme for the manager to use.**/
     private final Charset charset = StandardCharsets.UTF_16LE;
-    
+
     /**
      * Creates a new encryption manager. Note that the provided key will be erased after the manager finishes initializing.
      * @param key Char array containing the secret key that the archive should use for encrypting/decrypting.
-     * @return A new instance of an Encryption Manager initialized with the specified key. Or null if your system doesn't support the hash and encryption algorithms used internally.
+     * @param iv Byte array containing the initialization vector for the manager (starting value for the CTR counter). If null, the default IV is used.
+     * @return A new instance of an Encryption Manager initialized with the specified key. Or null if the manager couldn't be initialized correctly.
     **/
-    public static final EncryptionManager getInstance(char[] key)
+    public static final EncryptionManager getInstance(char[] key, byte[] iv)
     {
         try
         {
-            return new EncryptionManager(key);
+            return new EncryptionManager(key, iv);
         } catch(NoSuchAlgorithmException|NoSuchPaddingException unsupportedCryptoException)
         {
-            System.err.println("Your system doesn't provide support for either SHA-256 or AES-CTR.");
+            System.err.println("Your system doesn't provide support for either SHA-256 or AES256-ECB.");
             unsupportedCryptoException.printStackTrace();
         } catch(InvalidKeyException|InvalidAlgorithmParameterException cryptoParameterException)
         {
-            System.err.println("Parameter or Key provided was invalid!");
+            System.err.println("The provided key or IV was invalid!");
             cryptoParameterException.printStackTrace();
         }
         return null;
     }
 
-    private EncryptionManager(char[] key) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException
-    {
-        hashEngine1 = MessageDigest.getInstance("SHA-256");
-        hashEngine2 = MessageDigest.getInstance("SHA-256");
-        encryptionEngine = Cipher.getInstance("AES/CTR/NoPadding");
-        
-        CharBuffer cBuffer = CharBuffer.wrap(key);
-        ByteBuffer bBuffer = charset.encode(cBuffer);
-        encryptionEngine.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(hashEngine1.digest(bBuffer.array()), "AES"), new IvParameterSpec(iv.toByteArray()));
-        Arrays.fill(bBuffer.array(), (byte)255);
-        Arrays.fill(cBuffer.array(), '\255');
-        
-        randomEngine = SecureRandom.getInstanceStrong();
-    }
-    
-    public final void seek(long offset)
-    {
-        
-    }
-    
-    public final byte[] encryptStream(InputStream inputStream, OutputStream outputStream)
+    /**
+     * Creates a new encryption manager with a randomly generated key and iv
+     * @return A new instance of an Encryption Manager initialized with the specified key. Or null if the manager couldn't be initialized correctly.
+    **/
+    public final EncryptionManager getInstance(byte[] data)
     {
         try
         {
-            //Allocate arrays for holding the key and IV this stream will be encrypted with
-            byte[] streamKey = new byte[32];
-            byte[] streamIV = new byte[16];
-            //Generate a random key and IV for the stream
-            randomEngine.nextBytes(streamKey);
-            randomEngine.nextBytes(streamIV);
-            //Create a cipher for the stream and ensure the hash engines are flushed
-            Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
-            cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(streamKey, "AES"), new IvParameterSpec(streamIV));
-            
-            
-            //===Encrypt the stream===//
-        } catch(NoSuchAlgorithmException|NoSuchPaddingException unsupportedCryptoException)
+            byte[] b = new byte[48];
+            EncryptionManager encryptionManager = new EncryptionManager(b);
+            process(b);
+            System.arraycopy(b, 0, data, 0, 48);
+            return encryptionManager;
+        } catch(NoSuchAlgorithmException|NoSuchPaddingException|InvalidKeyException unsupportedCryptoException)
         {
-            System.err.println("Your system doesn't provide support for AES-CTR.");
+            System.err.println("Your system doesn't provide support for either SHA-256 or AES256-ECB.");
             unsupportedCryptoException.printStackTrace();
-        } catch(InvalidKeyException|InvalidAlgorithmParameterException cryptoParameterException)
+        } catch(IllegalBlockSizeException|BadPaddingException cryptoException)
         {
-            System.err.println("Parameter or Key provided was invalid!");
-            cryptoParameterException.printStackTrace();
+            System.err.println("Failed to encrypt new manager's parameters!");
+            cryptoException.printStackTrace();
         }
         return null;
+    }
+
+    //TODO
+    private EncryptionManager(char[] key, byte[] iv) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException
+    {
+        try
+        {
+            //Create the hash and encryption engines for the manager
+            hashEngine1 = MessageDigest.getInstance("SHA-256");
+            hashEngine2 = MessageDigest.getInstance("SHA-256");
+            encryptionEngine = Cipher.getInstance("AES/ECB/NoPadding");
+
+            //Initialize the cipher with a hash of the secret key
+            CharBuffer cBuffer = CharBuffer.wrap(key);
+            ByteBuffer bBuffer = charset.encode(cBuffer);
+            encryptionEngine.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(hashEngine1.digest(bBuffer.array()), "AES"));
+            //Overwrite any sensitive information stored in the buffers
+            Arrays.fill(cBuffer.array(), '\255');
+            Arrays.fill(bBuffer.array(), (byte)255);
+
+            //Initialize a secure RNG and a counter with the provided IV.
+            randomEngine = SecureRandom.getInstanceStrong();
+            currentPosition = 0;
+            if(iv == null)
+            {
+                counter = new byte[] {77, 97, 100, 105, 65, 117, 115, 116, 105, 110, 82, 97, 99, 104, 101, 108};
+            } else
+            if(iv.length == 16){
+                counter = iv;
+            } else{
+                throw new InvalidAlgorithmParameterException("IV must be exactly 16bytes in length.");
+            }
+        } finally{
+            //Ensure that the provided data is erased
+            Arrays.fill(key, '\255');
+            Arrays.fill(iv, (byte)255);
+        }
+    }
+
+    //TODO
+    private EncryptionManager(byte[] data) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException
+    {
+        //Create all the engines for the manager.
+        hashEngine1 = MessageDigest.getInstance("SHA-256");
+        hashEngine2 = MessageDigest.getInstance("SHA-256");
+        encryptionEngine = Cipher.getInstance("AES/ECB/NoPadding");
+        randomEngine = SecureRandom.getInstanceStrong();
+        currentPosition = 0;
+
+        //Allocate arrays for holding the key and counter
+        byte[] key = new byte[32];
+        counter = new byte[16];
+        //Generate a random key and IV for the counter
+        randomEngine.nextBytes(key);
+        randomEngine.nextBytes(counter);
+        //Initialize the encryption engine
+        encryptionEngine.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(key, "AES"));
+
+        //Copy the manager's parameters into the data field
+        if(data != null)
+        {
+            System.arraycopy(key, 0, data, 0, 32);
+            System.arraycopy(counter, 0, data, 32, 16);
+        }
+    }
+
+    /**
+     * Moves the manager's counter to the specified position.
+     * @param position The new position to move the counter to.
+    **/
+    public final void seek(long position)
+    {
+        seekRelative(position - currentPosition);
+    }
+
+    /**
+     * Moves the manager's counter forwards or backwards by the specified amount.
+     * @param offset The relative amount to offset the counter by.
+    **/
+    public final void seekRelative(long offset)
+    {
+        if(offset == 0)
+        {
+            return;
+        } else
+        if(offset == 1)
+        {
+            increment();
+        } else
+        if(offset == -1)
+        {
+            decrement();
+        } else{
+            //Convert the offset from a long to a byte array
+            byte[] b = new byte[8];
+            for(int i = 0; i < 8; i++)
+            {
+                b[i] = (byte)((offset >> (8 * i)) & 0xff);
+            }
+
+            if(offset > 0)
+            {
+                add(b);
+            } else{
+                subtract(b);
+            }
+        }
+    }
+
+    /**
+     * Shifts the manager's counter forward by 1.
+    **/
+    private final void increment()
+    {
+        for(int i = 0; i < counter.length; i++)
+        {
+            if(counter[i] == -128)
+            {
+                counter[i] = 0;
+            } else{
+                counter[i] = (byte)((counter[i] & 0xff) + 1);
+                break;
+            }
+        }
+    }
+
+    /**
+     * Shifts the manager's counter forward.
+     * @param b Byte array containing a base 256 number to shift the counter forward by. The length of this array must not be greater than 16 (length of the counter array).
+    **/
+    private final void add(byte[] b)
+    {
+        boolean carry = false;
+        int temp;
+
+        for(int i = 0; i < b.length; i++)
+        {
+            temp = (counter[i] & 0xff) + (b[i] & 0xff) + (carry? 1 : 0);
+            carry = (temp > 255);
+            counter[i] = (byte)(temp % 256);
+        }
+    }
+
+    /**
+     * Shifts the manager's counter backwards by 1.
+    **/
+    private final void decrement()
+    {
+        for(int i = 0; i < counter.length; i++)
+        {
+            if(counter[i] == 0)
+            {
+                counter[i] = -128;
+            } else{
+                counter[i] = (byte)((counter[i] & 0xff) - 1);
+                break;
+            }
+        }
+    }
+
+    /**
+     * Shifts the manager's counter backwards.
+     * @param b Byte array containing a base 256 number to shift the counter backwards by. The length of this array must not be greater than 16 (length of the counter array).
+    **/
+    private final void subtract(byte[] b)
+    {
+        boolean carry = false;
+        int temp;
+
+        for(int i = 0; i < b.length; i++)
+        {
+            temp = (counter[i] & 0xff) - (b[i] & 0xff) - (carry? 1 : 0);
+            carry = (temp < 0);
+            counter[i] = (byte)(temp % 256);
+        }
+    }
+
+    //TODO
+    public final void process(byte[] b) throws IllegalBlockSizeException, BadPaddingException
+    {
+        byte[] keyStream = null;
+        for(int i = 0; i < b.length; i++)
+        {
+            if((i % 16) == 0)
+            {
+                keyStream = encryptionEngine.doFinal(counter);
+                increment();
+            }
+            b[i] ^= keyStream[i];
+        }
+    }
+
+    //TODO
+    public final void process(InputStream inputStream, OutputStream outputStream, int bufferSize) throws IllegalBlockSizeException, BadPaddingException, IOException
+    {
+        byte[] buffer = new byte[(bufferSize / 16) * 16];
+        int count;
+        while((count = inputStream.read(buffer)) != -1)
+        {
+            process(buffer);
+            outputStream.write(buffer, 0, count);
+        }
+    }
+
+    //TODO
+    public final byte[][] processWithAuthenticate(byte[] b) throws IllegalBlockSizeException, BadPaddingException
+    {
+        //Reset the hash engine
+        hashEngine1.reset();
+        //Digest the plain-text
+        byte[] plainHash = hashEngine1.digest(b);
+
+        process(b);
+
+        //Reset the hash engine
+        hashEngine1.reset();
+        //Digest the cipher-text and return it with the plain hash
+        return new byte[][] {plainHash, hashEngine1.digest(b)};
+    }
+
+    //TODO
+    public final byte[][] processWithAuthenticate(InputStream inputStream, OutputStream outputStream, int bufferSize) throws IllegalBlockSizeException, BadPaddingException, IOException
+    {
+        //Reset the hash engines
+        hashEngine1.reset();
+        hashEngine2.reset();
+
+        byte[] buffer = new byte[(bufferSize / 16) * 16];
+        int count;
+        while((count = inputStream.read(buffer)) != -1)
+        {
+            hashEngine1.update(buffer);
+            process(buffer);
+            hashEngine2.update(buffer);
+            outputStream.write(buffer, 0, count);
+        }
+
+        //Digest the plain-text and cipher-text and return them
+        return new byte[][] {hashEngine1.digest(), hashEngine2.digest()};
     }
 }
--- src/com/insertcreativity/archive/Main.java
+++ src/com/insertcreativity/archive/Main.java
@@ -26,6 +26,26 @@ import javax.swing.JFrame;
 
 public class Main
 {
+    public static void main(String[] args)
+    {
+        switch(args[0])
+        {
+            case("init"):
+                initializeArchive(args);
+            break;
+                
+        }
+    }
+    
+    private static final initializeArchive(String[] args)
+    {
+        
+    }
+    
+    
+    
+    
+    
     private static final Charset UTF_8 = StandardCharsets.UTF_8;
     private static final int fileBufferSize = 65536;
     private static final int filenamePaddingLength = 120;
@@ -53,42 +73,52 @@ public class Main
         key = new SecretKeySpec(keyString.getBytes(UTF_8), "AES");
     }
     
-    private static final byte[] getVectorFromSize(long size)
+    /**
+     * Derives an initialization vector from a file length.
+     * @param length The length of the file to compute an IV for.
+     * @return A byte array representation of the file's IV.
+    **/
+    private final byte[] getIvFromLength(long length)
     {
-        //Allocate an array for storing the size bytes
-        byte[] sizeBytes = new byte[8];
+        //Allocate an array for storing the length as bytes
+        byte[] lengthBytes = new byte[8];
         //Convert the long into bytes and store them
         for(int i = 0; i < 8; i++)
         {
-            sizeBytes[i] = (byte)((size >> (i * 8)) & 0xff);
+            lengthBytes[i] = (byte)((length >> (i * 8)) & 0xff);
         }
         
         //Hash the bytes
-        byte[] hashBytes = hasher.digest(sizeBytes);
+        byte[] hashBytes = hasher.digest(lengthBytes);
         //Allocate an array for storing the initialization vector
-        byte[] resultBytes = new byte[16];
-        //Compute the vector bytes by adding together consecutive hash bytes
+        byte[] iv = new byte[16];
+        //Compute the IV bytes by adding together consecutive hash bytes
         for(int i = 0; i < 16; i++)
         {
-            resultBytes[i] = (byte)(hashBytes[2 * i] + hashBytes[(2 * i) + 1]);
+            iv[i] = (byte)(hashBytes[2 * i] + hashBytes[(2 * i) + 1]);
         }
         
-        return resultBytes;
+        return iv;
     }
     
-    private static final byte[] getVectorFromName(String name)
+    /**
+     * Derives an initialization vector from a file length.
+     * @param name The name of the file to compute an IV for.
+     * @return A byte array representation of the file's IV.
+    **/
+    private final byte[] getIvFromName(String name)
     {
-        //Hash the name string's bytes
+        //Encode the filename and hash it's bytes
         byte[] hashBytes = hasher.digest(name.getBytes(UTF_8));
         //Allocate an array for storing the initialization vector
-        byte[] resultBytes = new byte[16];
-        //Compute the vector bytes by adding together consecutive hash bytes
+        byte[] iv = new byte[16];
+        //Compute the IV bytes by adding together consecutive hash bytes
         for(int i = 0; i < 16; i++)
         {
-            resultBytes[i] = (byte)(hashBytes[2 * i] + hashBytes[(2 * i) + 1]);
+            iv[i] = (byte)(hashBytes[2 * i] + hashBytes[(2 * i) + 1]);
         }
         
-        return resultBytes;
+        return iv;
     }
     
     private static final void encrypt(File source, File dest) throws InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException, IOException
@@ -97,7 +127,7 @@ public class Main
         //Calculate the size of the file after encryption
         long size = ((source.isDirectory())? folderLength : ((source.length() / 16) + 1) * 16);
         //Initialize the cipher with a size-hash vector
-        cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(getVectorFromSize(size)));
+        cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(getIvFromLength(size)));
         //Pad the filename with '?' characters
         String paddedName = source.getName() + (new String(new char[filenamePaddingLength - source.getName().length()]).replaceAll("\0", "\\?"));
         //Encrypt the filename and convert it to a base64 string
@@ -126,7 +156,7 @@ public class Main
             
             //===Encrypt the file's content===//
             //Initialize the cipher with a name-hash vector
-            cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(getVectorFromName(source.getName())));
+            cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(getIvFromName(source.getName())));
             //Perform the encryption
             byte[] buffer = new byte[fileBufferSize];
             int count;
@@ -149,7 +179,7 @@ public class Main
         //Calculate the size of the file
         long size = ((source.isDirectory())? folderLength : source.length());
         //Initialize the cipher with a size-hash vector
-        cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(getVectorFromSize(size)));
+        cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(getIvFromLength(size)));
         //Decrypt the file name and remove any padding
         String name = new String(cipher.doFinal(Base64.getDecoder().decode(source.getName().replaceAll("-", "/"))), UTF_8);
         File decrypted = new File(dest, name.replaceAll("\\?", ""));
@@ -176,7 +206,7 @@ public class Main
             
             //===Decrypt the file's content===//
             //Initialize the cipher with a name-hash vector
-            cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(getVectorFromName(name)));
+            cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(getIvFromName(name)));
             //Perform the decryption
             byte[] buffer = new byte[fileBufferSize];
             int count;
@@ -191,5 +221,19 @@ public class Main
                 }
             }
         }
+    }
+
+    private static void createGraphics()
+    {
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = toolkit.getScreenSize();
+        
+        JFrame frame = new JFrame("Austin's Archive Manager");
+        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        
+        frame.setSize((screenSize.width * 3) / 4, (screenSize.height * 3) / 4);
+        frame.setLocationRelativeTo(null);
+        
+        frame.setVisible(true);
     }
 }
